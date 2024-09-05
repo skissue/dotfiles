ac18b0c842a2fce2fe919d2d53ebdbfa58934037
Ad <me@skissue.xyz>
2024-09-05 19:16:17 -0400

    feat(system/common) append "-impure" to impure builds

--- system/common/default.nix
+++ system/common/default.nix
@@ -19,7 +19,13 @@
 
   time.timeZone = lib.mkDefault "America/New_York";
 
-  system.configurationRevision = inputs.self.rev or "dirty";
+  system.configurationRevision =
+    (inputs.self.rev or "dirty")
+    + (
+      if (builtins.getEnv "PATH" != "")
+      then "-impure"
+      else ""
+    );
   # I'm using Flakes, there's no time I don't want Git
   environment.systemPackages = [pkgs.git];
 }
