0067c2bcfba79f7eeaa5b1b827e0f2379457fb08
Ad <me@skissue.xyz>
2024-09-07 15:29:59 -0400

    feat(home/emacs): popup minibuffer

--- home/emacs/config.el
+++ home/emacs/config.el
@@ -123,6 +123,41 @@
 (setopt enable-recursive-minibuffers t)
 (minibuffer-depth-indicate-mode)
 
+(setopt default-minibuffer-frame (make-frame '((window-system . pgtk)
+                                               (minibuffer . only)
+                                               (title . "MINIBUFFER"))))
+
+(defsubst my/toggle-minibuffer-workspace ()
+  "Toggle Hyprland's `minibuffer' special workspace."
+  (call-process "hyprctl" nil 0 nil
+                "dispatch" "togglespecialworkspace" "minibuffer"))
+
+(defun my/minibuffer-workspace-active-p ()
+  "Return non-nil if the minibuffer Hyprland workspace is currently active."
+  (let* ((json (with-temp-buffer
+                 ;; `hyprctl activeworkspace' ignores special workspaces.
+                 (call-process "hyprctl" nil t nil
+                               "activewindow" "-j")
+                 (goto-char (point-min))
+                 (json-parse-buffer :object-type 'alist)))
+         (workspace (map-nested-elt json '(workspace name))))
+    (string= workspace "special:minibuffer")))
+
+(define-advice read-from-minibuffer (:around (fn &rest args) use-popup-frame)
+  "Activate a separate minibuffer frame while reading from the minibuffer."
+  (let ((orig-frame (selected-frame)))
+    (unwind-protect
+        (progn
+          ;; Could already be active if in a recursive minibuffer.
+          (unless (my/minibuffer-workspace-active-p)
+            (my/toggle-minibuffer-workspace))
+          (select-frame-set-input-focus default-minibuffer-frame)
+          (apply fn args))
+      (when (and (zerop (minibuffer-depth))
+                 (my/minibuffer-workspace-active-p))
+        (my/toggle-minibuffer-workspace))
+      (select-frame-set-input-focus orig-frame))))
+
 (setopt comint-prompt-read-only t)
 
 (setopt compilation-always-kill t
@@ -407,7 +442,7 @@ it by adjusting the return value of
            ("M-P" . vertico-repeat-previous)
            ("M-N" . vertico-repeat-nex))
 
-(setopt vertico-count 15
+(setopt vertico-count 30
         vertico-cycle t
         vertico-resize nil
         vertico-quick1 "arstneio"
