1ccc50c34d44b6ce0233443767ec18d6e2c4855d
Ad <me@skissue.xyz>
2024-09-10 09:15:47 -0400

    feat(system/desktop/gaming): add notifications for gamemode

--- system/desktop/gaming/default.nix
+++ system/desktop/gaming/default.nix
@@ -9,6 +9,14 @@
           ];
       };
     };
-    gamemode.enable = true;
+    gamemode = {
+      enable = true;
+      settings = {
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        };
+      };
+    };
   };
 }
