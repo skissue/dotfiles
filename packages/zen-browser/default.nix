2e377cec2dd82cf6bdf88c499776efbea8583de1
Ad <me@skissue.xyz>
2024-10-10 20:44:03 -0400

    bump(packages/zen-browser): v1.0.1-a.8

--- packages/zen-browser/default.nix
+++ packages/zen-browser/default.nix
@@ -59,11 +59,11 @@
 in
   stdenv.mkDerivation rec {
     pname = "zen-browser-bin";
-    version = "1.0.1-a.7";
+    version = "1.0.1-a.8";
 
     src = fetchzip {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-      hash = "sha256-0EYfH8hFStQMpxn5YZ/EP4vX8sn4bGmWobpZgygTi7Y=";
+      hash = "sha256-VVeRN22uNerHPutKawVRYPn7GjrlEBotKzcPt3d3vF8=";
     };
 
     desktopItems = [
