cef3d4d2b04dddc963dedb3e5e08085a0a961bf7
Ad <me@skissue.xyz>
2024-10-11 21:49:25 -0400

    feat(system/networking/tailscale): adapt to new dns setup

--- system/networking/tailscale/default.nix
+++ system/networking/tailscale/default.nix
@@ -12,20 +12,14 @@
     enable = true;
     package = pkgs.tailscale.overrideAttrs (oldAttrs: {
       patches = [
-        # I don't want Tailscale to swallow all DNS traffic, but I want
-        # to keep `override_local_dns` on for mobile devices. So, patch
-        # Tailscale to never ask systemd-resolved for a `~.` domain.
+        # I don't want Tailscale to swallow all DNS traffic, but I still want it
+        #  to accept DNS settings for MagicDNS. So, patch Tailscale to never ask
+        #  systemd-resolved for a `~.` domain.
         ./no-all-route.patch
         # Limit the IPv4 CGNAT range to the range that I
         # use. Specifically restricted to not interfere with the IPv4
         # CGNAT range that Mullvad uses for DNS (100.64.0.0/24).
         ./limit-ipv4-cgnat.patch
-        # Horrendous, horrible, hardcoded hack to request the base server domain
-        # as a DNS search domain with systemd-resolved. This isn't usually
-        # necessary, since Tailscale requests the root domain already, but since
-        # we patch it to *not* do that, we need to request the server's domain
-        # instead, else addresses like `media.adtailnet` won't resolve.
-        ./request-base-dns-domain.patch
       ];
     });
     openFirewall = true;
