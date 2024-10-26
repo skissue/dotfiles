6eb7655d7ab5fa86ca9180ffc3f2b75c4d44e7ca
Ad <me@skissue.xyz>
2024-10-26 10:25:15 -0400

    bump(hosts/rendezvous): nextcloud30

--- hosts/rendezvous/nextcloud/default.nix
+++ hosts/rendezvous/nextcloud/default.nix
@@ -13,7 +13,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud30;
     https = true;
     hostName = "cloud.${private.domain.personal}";
 
