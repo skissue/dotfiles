eae05a9efd3edfd7cb7653b75d04f13724831338
Ad <me@skissue.xyz>
2024-11-07 15:31:47 -0500

    feat(system/desktop/gaming): add a no-gaming specialisation
    Little experiment to see if this helps in times of dire requirement for focus ¯\_(ツ)_/¯.

--- system/desktop/gaming/default.nix
+++ system/desktop/gaming/default.nix
@@ -1,25 +1,44 @@
-{pkgs, ...}: {
-  programs = {
-    steam = {
-      enable = true;
-      package = pkgs.steam.override {
-        extraLibraries = ps:
-          with ps; [
-            gamemode
-          ];
-      };
-    };
-    gamemode = {
-      enable = true;
-      settings = {
-        custom = {
-          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.my.enableGaming = lib.mkOption {
+    default = true;
+    type = lib.types.bool;
+    description = "Enable gaming-related software";
+  };
+  config = lib.mkMerge [
+    (lib.mkIf config.my.enableGaming {
+      programs = {
+        steam = {
+          enable = true;
+          package = pkgs.steam.override {
+            extraLibraries = ps:
+              with ps; [
+                gamemode
+              ];
+          };
+        };
+        gamemode = {
+          enable = true;
+          settings = {
+            custom = {
+              start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+              end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+            };
+          };
+        };
+        gamescope = {
+          enable = true;
         };
       };
-    };
-    gamescope = {
-      enable = true;
-    };
-  };
+    })
+    {
+      specialisation.no-gaming.configuration = {
+        my.enableGaming = false;
+      };
+    }
+  ];
 }
