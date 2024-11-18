0eafd2158a7e51d1b4051a94f9b940494a977d02
Ad <me@skissue.xyz>
2024-11-18 18:49:55 -0500

    feat(home/cli): add nix-output-monitor

--- home/cli/nix/default.nix
+++ home/cli/nix/default.nix
@@ -1,7 +1,13 @@
-{inputs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
     inputs.nix-index.hmModules.nix-index
   ];
 
   programs.nix-index-database.comma.enable = true;
+
+  home.packages = with pkgs; [nix-output-monitor];
 }
