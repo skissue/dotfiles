b9b6d2687aa2fe98393c591b6de8bcd384333944
Ad <me@skissue.xyz>
2024-11-24 11:29:34 -0500

    fix(hosts/nightcrawler): no need to lock with suspend now

--- hosts/nightcrawler/home/default.nix
+++ hosts/nightcrawler/home/default.nix
@@ -6,6 +6,6 @@
 
     monitor = eDP-1,2256x1504@60,0x0,1.566667
 
-    bindl = ,switch:on:Lid Switch,exec,hyprlock & systemctl suspend
+    bindl = ,switch:on:Lid Switch,exec,systemctl suspend
   '';
 }
