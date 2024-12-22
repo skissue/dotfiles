1baf1dbbd7179e7b9bcd5a47bb03093f6803ec20
Ad <me@skissue.xyz>
2024-12-21 19:19:20 -0500

    refactor(home/emacs): cleanup envrc config

--- home/emacs/config.el
+++ home/emacs/config.el
@@ -741,7 +741,6 @@ uses the symbol name as the default description, as well as a
 
 (envrc-global-mode)
 
-;; Force org-babel blocks to inherit their buffer's environment.
 (after! org
   (advice-add #'org-babel-execute-src-block :around #'envrc-propagate-environment))
 
