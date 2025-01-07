c4195b1e1b003c1dedf19660cda9115d07376824
Ad <me@skissue.xyz>
2025-01-07 17:35:46 -0500

    feat(home/hyprland,system/desktop): remove gnome-keyring
    Gonna try using just KeePassXC as my secret service, we'll see how it goes ¯\_(ツ)_/¯.

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -3,10 +3,6 @@
 
   services.logind.powerKey = "suspend";
 
-  # Keyring
-  services.gnome.gnome-keyring.enable = true;
-  programs.seahorse.enable = true;
-
   # Emacs ispell
   environment.wordlist.enable = true;
 
--- system/desktop/greetd/default.nix
+++ system/desktop/greetd/default.nix
@@ -8,6 +8,4 @@
     vt = 2;
     settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user-session --user-menu --asterisks --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
   };
-
-  security.pam.services.greetd.enableGnomeKeyring = true;
 }
