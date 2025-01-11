be8b097c8b1fe6cd87a8ddcf8a2fb224eb5d2ef1
Ad <me@skissue.xyz>
2025-01-11 11:16:16 -0500

    feat(home/emacs): tweak scroll settings for ultra-scroll
    Apparently, setting `scroll-margin` to anything other than 0 automatically makes
    scrolling a miserable experience. Which is quite a shame, since I really like
    the concept, and particularly enjoyed the similar behavior in Neovim 😐.

--- home/emacs/config.el
+++ home/emacs/config.el
@@ -252,12 +252,21 @@ where it was when you previously visited the same file."
   ";" #'negative-argument
   "U" #'meow-universal-argument)
 
-(setq-default scroll-conservatively 101
-              scroll-margin 4
-              scroll-preserve-screen-position t
-              next-screen-context-lines 4
-              auto-window-vscroll nil
-              fast-but-imprecise-scrolling t)
+(setq scroll-conservatively 101
+      scroll-margin 0
+      scroll-preserve-screen-position t
+      next-screen-context-lines 4
+      redisplay-skip-fontification-on-input t
+      fast-but-imprecise-scrolling t)
+
+(add-hook 'my/first-input-hook #'ultra-scroll-mode)
+
+(after! pixel-scroll
+  (bind-keys :map pixel-scroll-precision-mode-map
+             ;; For some reason, these are swapped 💀.
+             ([remap scroll-up-command] . pixel-scroll-interpolate-down)
+             ([remap scroll-down-command] . pixel-scroll-interpolate-up))
+  (setq pixel-scroll-precision-interpolate-page t))
 
 (setq-default indent-tabs-mode nil
               sentence-end-double-space nil
@@ -331,8 +340,6 @@ where it was when you previously visited the same file."
 (define-advice elisp--company-doc-buffer (:around (fn &rest args) popup-fix)
   (let (display-buffer-base-action)
     (apply fn args)))
-
-(add-hook 'my/first-input-hook #'ultra-scroll-mode)
 
 (global-visual-line-mode)
 
