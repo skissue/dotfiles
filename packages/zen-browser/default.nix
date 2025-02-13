03f96569d946771884bc11e0609c6de57f277cfa
Ad <me@skissue.xyz>
2025-02-12 21:29:57 -0500

    bump(packages/zen-browser): v1.7.6b

--- packages/zen-browser/default.nix
+++ packages/zen-browser/default.nix
@@ -59,7 +59,7 @@
 in
   stdenv.mkDerivation rec {
     pname = "zen-browser-bin";
-    version = "1.7.4b";
+    version = "1.7.6b";
 
     src = fetchzip {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
