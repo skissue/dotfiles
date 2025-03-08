aba295e22ce721d37b67ab99c94233309f83cc17
Ad <me@skissue.xyz>
2025-03-07 22:43:40 -0500

    fix(system/desktop/fonts): use Atkinson Hyperlegible Next

--- system/desktop/fonts/default.nix
+++ system/desktop/fonts/default.nix
@@ -14,9 +14,7 @@
     my.monolisa
     nerd-fonts.symbols-only
     (google-fonts.override {fonts = ["Karla"];})
-    atkinson-hyperlegible
-    # TODO Switch to this when nixpkgs input is updated.
-    # atkinson-hyperlegible-next
+    atkinson-hyperlegible-next
     twitter-color-emoji
   ];
 }
