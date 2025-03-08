59dd851787548f1d9c8544448d765b674c6bba29
Ad <me@skissue.xyz>
2025-03-08 11:05:15 -0500

    feat(system/desktop/fonts): remove iosevka
    🫡

--- system/desktop/fonts/default.nix
+++ system/desktop/fonts/default.nix
@@ -7,10 +7,6 @@
     gyre-fonts
     unifont
 
-    (iosevka-bin.override {variant = "SS18";})
-    (iosevka-bin.override {variant = "Slab";})
-    (iosevka-bin.override {variant = "Aile";})
-    (iosevka-bin.override {variant = "Etoile";})
     my.monolisa
     nerd-fonts.symbols-only
     (google-fonts.override {fonts = ["Karla"];})
