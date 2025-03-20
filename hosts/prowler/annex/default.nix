45bf034dfbecbcd397c2fac1f22e3687dfe0f680
Ad <me@skissue.xyz>
2025-03-20 17:06:16 -0400

    fix(hosts/prowler): add git-annex-backend-XBLAKE3

--- hosts/prowler/annex/default.nix
+++ hosts/prowler/annex/default.nix
@@ -1,12 +1,17 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: let
   user = config.users.users.${config.my.user.name};
 in {
-  # For `git-annex-shell`.
-  environment.systemPackages = with pkgs; [git-annex];
+  environment.systemPackages = with pkgs; [
+    # For `git-annex-shell`.
+    git-annex
+    # `git-annex` complains if it's not present.
+    inputs.git-annex-backend-XBLAKE3.packages.${pkgs.system}.default
+  ];
 
   # Data here (presumably) gets backed up from other places, so leave it in
   # local.
