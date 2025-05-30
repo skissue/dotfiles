197b3a5bebc6fc93e1729842b0f5facaca69ebf6
Ad <me@skissue.xyz>
2025-05-30 12:17:01 -0400

    fix(flake): explicit bleeding lix input
    Despite using bleeding lix-module, its own lix input doesn't seem to be updated,
    which causes *it* to warn about mismatched versions. So, let's just pull in the
    bleeding version ourselves. This seems to also be the approach recommended by
    upstream for running bleeding Lix[1].
    
    [1] https://wiki.lix.systems/books/lix-contributors/page/running-lix-main#bkmrk-flakes

--- flake.nix
+++ flake.nix
@@ -15,9 +15,14 @@
 
     private.url = "git+ssh://ad@rendezvous/home/ad/dotfiles.private";
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     devshell = {
