00215a559cf97ec2da10c987c67da511ee7ce17c
Ad <me@skissue.xyz>
2025-07-01 12:19:00 -0400

    feat(home/niri): port former hypridle configuration

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -56,6 +56,26 @@
     };
   };
 
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+      };
+      listener = [
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
   home.sessionVariables = {
     "XDG_SESSION_TYPE" = "wayland";
     "QT_QPA_PLATFORM" = "wayland;xcb";
