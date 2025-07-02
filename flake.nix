d79627b9bc65050d65cfe7c5fa26a3a7f5014e02
Ad <me@skissue.xyz>
2025-07-01 20:07:21 -0400

    feat(system/common/nix): remove Lix
    Getting a build error on latest version, spent way too long trying to work
    around it, went down the rabbit hole of older versions, and eventually gave up
    on everything and decided it's not worth it. I don't even know what it's adding.
    But perhaps I shall return in the future… but fornow, not worth it 😔. Perhaps
    my days of bleeding-edge-everything are behind me now 🫡.

--- flake.nix
+++ flake.nix
@@ -15,16 +15,6 @@
 
     private.url = "git+ssh://ad@rendezvous/home/ad/dotfiles.private";
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
-
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
