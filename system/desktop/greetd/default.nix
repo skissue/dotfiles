e0de2add7299daf16c3656bb70680b8a22e76c37
Ad <me@skissue.xyz>
2025-08-06 09:38:49 -0400

    fix(system/desktop/greetd): remove setting for vt = 2
    This is no longer supported, nixpkgs wants to keep everything on VT 1 for
    consistency 👍.

--- system/desktop/greetd/default.nix
+++ system/desktop/greetd/default.nix
@@ -5,7 +5,6 @@
 }: {
   services.greetd = {
     enable = true;
-    vt = 2;
     settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user-session --user-menu --asterisks --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
   };
 }
