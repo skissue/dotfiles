87f6af6e26f73de6f44192db570f855d1461fdf1
Ad <me@skissue.xyz>
2025-11-11 19:14:44 -0500

    feat(home/programming): remove mold for Rust
    New versions now default to lld, which approaches the speed of mold with better reliability.

--- home/programming/rust.nix
+++ home/programming/rust.nix
@@ -1,10 +1,5 @@
-# Actual Rust tooling (`rustc`, `cargo`, etc.) is managed per-project with
+# Actual Rust tooling (rustc, cargo, etc.) is managed per-project with
 # devshells.
 {pkgs, ...}: {
-  home.packages = with pkgs; [rust-analyzer bacon mold];
-
-  home.file.".cargo/config.toml".text = ''
-    [target.x86_64-unknown-linux-gnu]
-    rustflags = ["-C", "link-arg=-fuse-ld=mold"]
-  '';
+  home.packages = with pkgs; [rust-analyzer bacon];
 }
