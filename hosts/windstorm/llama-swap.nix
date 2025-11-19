0c05556e9be50d6186921ceddea7b50511fb7550
Ad <me@skissue.xyz>
2025-11-18 20:09:57 -0500

    fix(hosts/windstorm): bump llama-swap model ttl to 5min
    2min was way too short ¯\_(ツ)_/¯.

--- hosts/windstorm/llama-swap.nix
+++ hosts/windstorm/llama-swap.nix
@@ -15,27 +15,27 @@
       models = {
         "qwen3-vl:8b" = {
           cmd = "\${server} -hf unsloth/Qwen3-VL-8B-Thinking-GGUF:Q8_K_XL --temp 1.0 --top-k 20 --top-p 0.95 --presence-penalty 0.0";
-          ttl = 120;
+          ttl = 300;
         };
         "qwen3-vl:8b-instruct" = {
           cmd = "\${server} -hf unsloth/Qwen3-VL-8B-Instruct-GGUF:Q8_K_XL --temp 0.7 --top-k 20 --top-p 0.8 --presence-penalty 1.5";
-          ttl = 120;
+          ttl = 300;
         };
         "qwen3:14b" = {
           cmd = "\${server} -hf unsloth/Qwen3-14B-GGUF:UD-Q6_K_XL --temp 0.6 --top-k 20 --top-p 0.95 --min-p 0 --presence-penalty 1.5";
-          ttl = 120;
+          ttl = 300;
         };
         "qwen3:30b" = {
           cmd = "\${server} -hf unsloth/Qwen3-30B-A3B-Thinking-2507-GGUF:UD-IQ3_XXS --temp 0.6 --top-k 20 --top-p 0.95 --min-p 0 --presence-penalty 1.0";
-          ttl = 120;
+          ttl = 300;
         };
         "qwen3:30b-instruct" = {
           cmd = "\${server} -hf unsloth/Qwen3-30B-A3B-Instruct-2507-GGUF:UD-IQ3_XXS --temp 0.7 --top-k 20 --top-p 0.8 --min-p 0 --presence-penalty 1.0";
-          ttl = 120;
+          ttl = 300;
         };
         "qwen3-coder:30b" = {
           cmd = "\${server} -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:UD-IQ3_XXS --temp 0.7 --top-k 20 --top-p 0.8 --presence-penalty 1.05";
-          ttl = 120;
+          ttl = 300;
         };
       };
     };
