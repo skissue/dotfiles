65f8066a8566fcf84608c6609ff911c89bb97b69
Ad <me@skissue.xyz>
2025-12-12 12:34:43 -0500

    fix(hosts/prowler): persist dotfiles.private repo

--- hosts/prowler/default.nix
+++ hosts/prowler/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   mkModulesList,
   pkgs,
   ...
@@ -44,6 +45,20 @@
       "fd7a:115c:a1e0:b1a:0:1:a00:0/120"
     ];
   };
+
+  # Trying to fetch the information via
+  # `config.users.users.${config.my.user.name}` results in infinite recursion.
+  # Don't ask me why, I have zero idea.
+  my.persist.local.directories = let
+    user = config.my.user.name;
+  in [
+    {
+      directory = "/home/${user}/dotfiles.private";
+      user = user;
+      group = "users";
+      mode = "750";
+    }
+  ];
 
   # State version (copy from auto-generated configuration.nix during install)
   # This value determines the NixOS release from which the default
