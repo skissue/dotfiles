3620707f7e99de578401282b25a9b319e137ec6a
Ad <me@skissue.xyz>
2025-12-30 12:43:07 -0500

    refactor(system/desktop/gaming): remove no-gaming specialisation

--- system/desktop/gaming/default.nix
+++ system/desktop/gaming/default.nix
@@ -1,46 +1,27 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  options.my.enableGaming = lib.mkOption {
-    default = true;
-    type = lib.types.bool;
-    description = "Enable gaming-related software";
+{pkgs, ...}: {
+  programs = {
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraLibraries = ps:
+          with ps; [
+            gamemode
+          ];
+      };
+      extraCompatPackages = with pkgs; [proton-ge-bin steamtinkerlaunch];
+    };
+    gamemode = {
+      enable = true;
+      settings = {
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        };
+      };
+    };
+    gamescope = {
+      enable = true;
+      package = pkgs.gamescope_git;
+    };
   };
-  config = lib.mkMerge [
-    (lib.mkIf config.my.enableGaming {
-      programs = {
-        steam = {
-          enable = true;
-          package = pkgs.steam.override {
-            extraLibraries = ps:
-              with ps; [
-                gamemode
-              ];
-          };
-          extraCompatPackages = with pkgs; [proton-ge-bin steamtinkerlaunch];
-        };
-        gamemode = {
-          enable = true;
-          settings = {
-            custom = {
-              start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-              end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-            };
-          };
-        };
-        gamescope = {
-          enable = true;
-          package = pkgs.gamescope_git;
-        };
-      };
-    })
-    {
-      specialisation.no-gaming.configuration = {
-        my.enableGaming = false;
-      };
-    }
-  ];
 }
