868df7a9004bfb20c2a44263366cdc2a446eebaa
Ad <me@skissue.xyz>
2026-01-03 14:41:03 -0500

    feat(system/common/ssh): remove old ssh keys

--- system/common/ssh.nix
+++ system/common/ssh.nix
@@ -16,11 +16,9 @@
   };
 
   users.users.${config.my.user.name}.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRMZR0lD/uE1zpUIDbBLJoQKMnbpkTO1Y2EbEhfG38u Ad"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII8YKBCF+gldnk4GnyibOqjSRDgZr0YTPHiyJeqIcdUO"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILSQO88S8m8AtbWqZ7B6FOiSpnbsKhtqwlLwLvO7E4xe ad@flashpoint"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID9JGTxwDN1T9OGFqH8Ixmswn2i0XgUmWhlVfk+PbAScAAAABHNzaDo= Ad"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILHCtSdMw5fIO3e2B5ZNdAbIucoBZ6N/77tm1QYXFmZsAAAABHNzaDo= Ad"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILSQO88S8m8AtbWqZ7B6FOiSpnbsKhtqwlLwLvO7E4xe ad@flashpoint"
   ];
 
   my.persist.local.files = [
