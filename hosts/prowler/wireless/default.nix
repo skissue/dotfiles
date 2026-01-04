4c2f0e9d0024fbe601a0a622e5687581ec718367
Ad <me@skissue.xyz>
2026-01-04 12:58:06 -0500

    fix(hosts/prowler/wireless): user/group for wireless secret

--- hosts/prowler/wireless/default.nix
+++ hosts/prowler/wireless/default.nix
@@ -1,5 +1,9 @@
 {config, ...}: {
-  sops.secrets.prowler-wireless-env = {};
+  sops.secrets.prowler-wireless-env = {
+    # Hardcoded: https://github.com/NixOS/nixpkgs/blob/fb7944c166a3b630f177938e478f0378e64ce108/nixos/modules/services/networking/wpa_supplicant.nix#L689-L694
+    owner = "wpa_supplicant";
+    group = "wpa_supplicant";
+  };
 
   networking.wireless = {
     enable = true;
