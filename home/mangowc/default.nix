6c4b84d0719694cb4f5e021b29e286c7d1b1b39c
Ad <me@skissue.xyz>
2026-01-19 10:06:11 -0500

    fix(home/mangowc): add mako

--- home/mangowc/default.nix
+++ home/mangowc/default.nix
@@ -21,22 +21,22 @@
 
   home.sessionPath = [(toString ./scripts)];
 
-  # xdg.configFile."mako/config".text = ''
-  #   default-timeout=5000
-  #   group-by=app-name,summary
-  #   layer=overlay
-  #
-  #   font=Atkinson Hyperlegible Next 16
-  #   background-color=#24273ad9
-  #   text-color=#cad3f5
-  #
-  #   width=500
-  #   height=200
-  #   border-size=0
-  #
-  #   on-notify=exec pw-play ${./zoom.ogg}
-  #   on-button-middle=exec makoctl menu -n "$id" $DMENU -p 'Select action: '
-  # '';
+  xdg.configFile."mako/config".text = ''
+    default-timeout=5000
+    group-by=app-name,summary
+    layer=overlay
+  
+    font=Atkinson Hyperlegible Next 16
+    background-color=#24273ad9
+    text-color=#cad3f5
+  
+    width=500
+    height=200
+    border-size=0
+  
+    on-notify=exec pw-play ${./zoom.ogg}
+    on-button-middle=exec makoctl menu -n "$id" $DMENU -p 'Select action: '
+  '';
 
   programs.hyprlock = {
     enable = true;
