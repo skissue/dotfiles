f2082371be7a128bb4c628a8e7f8afd839036be4
Ad <me@skissue.xyz>
2026-02-01 21:33:30 -0500

    feat(system/networking): enable networkd

--- system/networking/default.nix
+++ system/networking/default.nix
@@ -6,8 +6,8 @@
   imports = [./tailscale];
 
   networking = {
+    useNetworkd = true;
     hostName = host;
-    useDHCP = lib.mkDefault true;
     nameservers = ["9.9.9.9#dns.quad9.net" "149.112.112.112#dns.quad9.net" "2620:fe::fe#dns.quad9.net" "2620:fe::9#dns.quad9.net"];
   };
 
@@ -16,9 +16,8 @@
     flushRuleset = false; # Breaks active Mullvad/Tailscale connection rules
   };
 
-  # Acts as cache, layering on top of Tailscale's DNS server
   services.resolved = {
-    enable = lib.mkDefault true;
+    enable = true;
     settings.Resolve = {
       FallbackDNS = [""]; # Never fallback (don't want to use default servers)
       DNSOverTLS = "true";
