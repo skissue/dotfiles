db9b63c7e85366f53010e329f7d3a1a6b4f003a6
Ad <me@skissue.xyz>
2026-02-10 10:25:29 -0500

    feat(system/networking/tailscale): remove patches

--- system/networking/tailscale/default.nix
+++ system/networking/tailscale/default.nix
@@ -12,23 +12,6 @@ in {
 
   services.tailscale = {
     enable = true;
-    package = pkgs.tailscale.overrideAttrs (oldAttrs: {
-      patches =
-        oldAttrs.patches or []
-        ++ [
-          # I don't want Tailscale to swallow all DNS traffic, but I still want it
-          # to accept DNS settings for MagicDNS. So, patch Tailscale to never ask
-          # systemd-resolved for a `~.` domain.
-          ./no-all-route.patch
-          # Limit the IPv4 CGNAT range to the range that I
-          # use. Specifically restricted to not interfere with the IPv4
-          # CGNAT range that Mullvad uses for DNS (100.64.0.0/24).
-          ./limit-ipv4-cgnat.patch
-        ];
-
-      # A lot of tests fail with a custom CGNAT range.
-      doCheck = false;
-    });
     openFirewall = true;
     authKeyFile = config.sops.secrets.tailscale-auth-key.path;
     extraDaemonFlags = ["--no-logs-no-support"];
