1aebde66c38c8cef954a2c8829a3e4cfafb0f409
Ad <me@skissue.xyz>
2026-04-24 18:44:44 -0400

    fix(system/desktop): new man cache option

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -17,7 +17,7 @@
   environment.wordlist.enable = true;
 
   # `man -k` and other manual searchers (like Emacs)
-  documentation.man.generateCaches = true;
+  documentation.man.cache.enable = true;
 
   # For `setcap` permissions. Does NOT install the actual executable.
   programs.gpu-screen-recorder.enable = true;
