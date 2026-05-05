fd784f6621ab5eed7bb5a8d9b865b95a8688515d
Ad <me@skissue.xyz>
2026-05-04 21:22:38 -0400

    fix(hosts/windstorm): bad behaving usb device

--- hosts/windstorm/default.nix
+++ hosts/windstorm/default.nix
@@ -43,6 +43,8 @@
   networking.firewall.allowedTCPPorts = [21000 21013];
   networking.firewall.allowedUDPPorts = [21003];
 
+  boot.kernelParams = [ "usbcore.quirks=303a:8253:m" ];
+
   # State version (copy from auto-generated configuration.nix during install)
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
