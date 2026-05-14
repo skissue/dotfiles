37b74cbba71a8f037c865ca0c8ea95eea9364bc3
Ad <me@skissue.xyz>
2026-05-14 14:40:52 -0400

    feat(home/cursors): catppuccin mocha pink

--- home/cursors/default.nix
+++ home/cursors/default.nix
@@ -4,15 +4,8 @@
   ...
 }: {
   home.pointerCursor = {
-    # Only include the variants I care about
-    package = pkgs.nordzy-cursor-theme.overrideAttrs (_: {
-      installPhase = ''
-        mkdir -p $out/share/icons
-        cp -r xcursors/Nordzy-cursors-white $out/share/icons/
-        cp -r hyprcursors/themes/Nordzy-hyprcursors-white $out/share/icons/
-      '';
-    });
-    name = "Nordzy-cursors-white";
+    package = pkgs.catppuccin-cursors.mochaPink;
+    name = "catppuccin-mocha-pink-cursors";
     size = 36;
     gtk.enable = true;
   };
