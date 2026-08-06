af5713df26ffa72d93ee4d1d7415d5a6a8c13611
Ad <me@skissue.xyz>
2026-08-06 09:20:49 -0400

    feat(home/quickshell): build shaders in nix

--- home/quickshell/default.nix
+++ home/quickshell/default.nix
@@ -2,8 +2,20 @@
   pkgs,
   inputs,
   mutable-link,
+  osConfig,
   ...
-}: {
+}: let
+  compiledShaders =
+    pkgs.runCommandLocal "quickshell-shaders" {
+      nativeBuildInputs = [pkgs.kdePackages.qtshadertools];
+    } ''
+      mkdir -p "$out/shaders"
+
+      for shader in ${./config/shaders}/*.frag; do
+        qsb --qt6 "$shader" -o "$out/shaders/$(basename "$shader").qsb"
+      done
+    '';
+in {
   home.packages = with pkgs; [
     (inputs.quickshell.packages.${stdenv.hostPlatform.system}.default.withModules [
       kdePackages.qtmultimedia
@@ -13,7 +25,19 @@
   ];
 
   xdg.configFile."quickshell" = {
-    source = mutable-link ./config;
+    # Keep mutable builds pointed directly at the working tree. In-tree QSB
+    # files are intentionally used there so shader edits can be tested live.
+    source =
+      if osConfig.my.enable-mutable-links
+      then mutable-link ./config
+      else
+        pkgs.symlinkJoin {
+          name = "quickshell-config";
+          paths = [
+            ./config
+            compiledShaders
+          ];
+        };
     recursive = true;
   };
 
