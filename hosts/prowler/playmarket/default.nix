25f65e372c5871a2efd142627b9f6b3e5b5487b6
Ad <me@skissue.xyz>
2026-08-10 10:14:35 -0400

    feat(hosts/prowler): receive playmarket traffic from iridescent

--- hosts/prowler/playmarket/default.nix
+++ hosts/prowler/playmarket/default.nix
@@ -1,40 +1,39 @@
 {
   config,
   inputs,
+  lib,
   private,
   ...
 }: let
   cfg = config.services.playmarket;
-  auth = config.services.tailscaleAuth;
   domain = "playmarket.${private.domain.wasteoftime}";
+  iridescentAddresses = [
+    "100.72.86.116"
+    "fd7a:115c:a1e0:f811:19ba:3d4f:a16e:e3c6"
+  ];
+  iridescentAddressesString = lib.concatStringsSep " " iridescentAddresses;
 in {
   imports = [inputs.playmarket.nixosModules.default];
 
   services.playmarket.enable = true;
   services.playmarket.port = 8889;
 
-  services.tailscaleAuth.enable = true;
-  users.users.${config.services.caddy.user}.extraGroups = [auth.group];
+  services.caddy = {
+    globalConfig = lib.mkAfter ''
+      servers {
+        trusted_proxies static ${iridescentAddressesString}
+        trusted_proxies_strict
+      }
+    '';
 
-  security.acme.certs.wasteoftime.domain = "*.${private.domain.wasteoftime}";
+    virtualHosts."http://${domain}".extraConfig = ''
+      @not-iridescent not remote_ip ${iridescentAddressesString}
+      respond @not-iridescent 403
 
-  services.caddy.virtualHosts.${domain} = {
-    useACMEHost = "wasteoftime";
-    extraConfig = ''
-      route {
-        request_header -X-Auth-User
+      request_header -X-Auth-User
 
-        forward_auth unix/${auth.socketPath} {
-          uri /auth
-          header_up Remote-Addr {remote_host}
-          header_up Remote-Port {remote_port}
-          header_up Original-URI {uri}
-          copy_headers {
-            Tailscale-User>X-Auth-User
-          }
-        }
-
-        reverse_proxy http://127.0.0.1:${toString cfg.port}
+      reverse_proxy http://127.0.0.1:${toString cfg.port} {
+        header_up X-Real-IP {client_ip}
       }
     '';
   };
