104c517a9a0c3cf26a9824b53589cc48c9f2c34b
Ad <me@skissue.xyz>
2026-09-15 12:58:52 -0400

    feat(system/common): add mutable-link to module arguments

--- system/common/mutable-links.nix
+++ system/common/mutable-links.nix
@@ -1,11 +1,29 @@
 # NixOS specialisations setup for toggling mutable links. See
 # https://wiki.nixos.org/wiki/Specialisation for details.
-{lib, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}: {
   options = {
     my.enable-mutable-links = lib.mkEnableOption "mutable-links";
   };
 
   config = {
+    _module.args.mutable-link = path:
+      assert lib.types.path.check path; let
+        relative = lib.strings.removePrefix (toString self) (toString path);
+        full = "/etc/dotfiles" + relative;
+      in
+        if config.my.enable-mutable-links
+        then
+          pkgs.runCommandLocal "mutable-link-${relative}" {inherit full;} ''
+            ln -s "$full" "$out"
+          ''
+        else path;
+
     specialisation.mutable-links.configuration = {
       my.enable-mutable-links = true;
       # For `nh`; see
